a1504aa38d65797ac937b0f03c598951cdb16673
Manfred7 <Manfred7@yandex.ru>
2022-11-30 23:23:12 +0300

    Проп size имеет два возможных значения, medium и large (по умолчанию medium). Определяет размер кнопки.
    Проп variant имеет два возможных значения, filled и outlined (по умолчанию filled). Определяет оформление кнопки.
    
    Проп color имеет два возможных значения, orange и green (по умолчанию orange). Определяет цвет кнопки.

--- src/App.js
+++ src/App.js
@@ -1,20 +1,61 @@
-
-import {ButtonColor, ButtonSize, ButtonVariant, StyledButton} from "./elements/button";
-
+import {ButtonColorKind, ButtonSize, ButtonVariant, StyledButton} from "./elements/button";
 
 
 function App() {
-  return (
-    <div className="App">
-      <StyledButton size={ButtonSize.MEDIUM} variant={ButtonVariant.FILLED} >Кнопка</StyledButton>
-        <StyledButton size={ButtonSize.MEDIUM} variant={ButtonVariant.OUTLINED} >Кнопка</StyledButton>
-        <StyledButton size={ButtonSize.LARGE} variant={ButtonVariant.OUTLINED}  color={ButtonColor.GREEN}>Кнопка green</StyledButton>
+    return (
+        <div className="App">
+            <div>
+                <StyledButton size={ButtonSize.MEDIUM}
+                              variant={ButtonVariant.FILLED}
+                              color={ButtonColorKind.GREEN}>
+                    Кнопка
+                </StyledButton>
+                <StyledButton size={ButtonSize.MEDIUM}
+                              variant={ButtonVariant.OUTLINED}
+                              color={ButtonColorKind.GREEN}>
+                    Кнопка
+                </StyledButton>
+                <StyledButton size={ButtonSize.LARGE}
+                              color={ButtonColorKind.GREEN}
+                              disabled={false}>
+                    Кнопка
+                </StyledButton>
+                <StyledButton size={ButtonSize.LARGE}
+                              color={ButtonColorKind.GREEN}
+                              disabled={true}>
+                    Кнопка
+                </StyledButton>
+            </div>
 
-      <StyledButton size={ButtonSize.LARGE} color={ButtonColor.GREEN}>Кнопка green</StyledButton>
+            <div>
+                <StyledButton size={ButtonSize.MEDIUM}
+                              variant={ButtonVariant.FILLED}
+                              color={ButtonColorKind.ORANGE}>
+                    Кнопка
+                </StyledButton>
+                <StyledButton size={ButtonSize.MEDIUM}
+                              variant={ButtonVariant.OUTLINED}
+                              color={ButtonColorKind.ORANGE}>
+                    Кнопка
+                </StyledButton>
+
+                <StyledButton size={ButtonSize.LARGE}
+                              color={ButtonColorKind.ORANGE}
+                              disabled={false}>
+                    Кнопка
+                </StyledButton>
+
+                <StyledButton size={ButtonSize.LARGE}
+                              variant={ButtonVariant.OUTLINED}
+                              color={ButtonColorKind.ORANGE}
+                              disabled={true}>
+                    Кнопка
+                </StyledButton>
+            </div>
 
 
-    </div>
-  );
+        </div>
+    );
 }
 
 export default App;
--- src/elements/button.jsx
+++ src/elements/button.jsx
@@ -1,13 +1,12 @@
-// TODO реализовать компонент Button в этом файле
-import styled, {css} from 'styled-components';
+/*
+нужно разработать кнопку:
 
-const Color = {
-    TEXT: '#ffffff',
-    BACKGROUND: '#56aaf7',
-    BACKGROUND_HOVER_OR_FOCUS: '#8fc8fd',
-    BACKGROUND_ACTIVE: '#2891f1',
-    BACKGROUND_DISABLED: '#dfdfdf'
-};
+Проп size имеет два возможных значения, medium и large (по умолчанию medium). Определяет размер кнопки.
+Проп variant имеет два возможных значения, filled и outlined (по умолчанию filled). Определяет оформление кнопки.
+Проп color имеет два возможных значения, orange и green (по умолчанию orange). Определяет цвет кнопки.
+
+*/
+import styled, {css} from 'styled-components';
 
 const CursorVariants = {
     POINTER: 'pointer',
@@ -22,7 +21,7 @@ export const ButtonVariant = {
     FILLED: "filled",
     OUTLINED: "outlined"
 }
-export const ButtonColor = {
+export const ButtonColorKind = {
     ORANGE: "ORANGE",
     GREEN: "GREEN"
 
@@ -40,41 +39,125 @@ const ButtonSizeToCSS = {
     `
 };
 
-const ButtonColorToCSS = {
-    [ButtonColor.GREEN]: css`
-      background-color:#5FCB45 ;
-    `,
-    [ButtonColor.ORANGE]: css`
-      background-color: #FF833E;
-    `
+export const ButtonDetailColorTypes = {
+    COLOR: "COLOR",
+    HOVER_OR_FOCUS: "HOVER_OR_FOCUS",
+    ACTIVE: "ACTIVE",
+    DISABLED: "DISABLED",
+}
+
+const ButtonColors = {
+    [ButtonColorKind.GREEN]: {
+        [ButtonDetailColorTypes.COLOR]: `#5FCB45`,
+        [ButtonDetailColorTypes.HOVER_OR_FOCUS]: `#86DA71`,
+        [ButtonDetailColorTypes.ACTIVE]: `#42B026`,
+        [ButtonDetailColorTypes.DISABLED]: `#ADADAD`
+    },
+    [ButtonColorKind.ORANGE]: {
+        [ButtonDetailColorTypes.COLOR]: `#FF833E`,
+        [ButtonDetailColorTypes.HOVER_OR_FOCUS]: `#FF9F69`,
+        [ButtonDetailColorTypes.ACTIVE]: `#F06314`,
+        [ButtonDetailColorTypes.DISABLED]: `#ADADAD`
+    }
 };
+
+
 export const StyledButton = styled.button.attrs((props) => ({
     type: props.type || 'button',
     size: props.size || ButtonSize.MEDIUM,
     variant: props.variant || ButtonVariant.FILLED,
-    color: props.color || ButtonColor.ORANGE
+    color: props.color || ButtonColorKind.ORANGE
 }))`
 
-  ${({size}) => ButtonSizeToCSS[size]}
-
-  /*color: ${Color.TEXT};*/
-  ${ ({color})=>ButtonColorToCSS[color]};
   cursor: ${CursorVariants.POINTER};
-  //background: #5FCB45;
   border-radius: 4px;
 
+  ${({size}) => ButtonSizeToCSS[size]}
+  ${({color, variant}) => {
 
-  &:hover,
-  &:focus {
-    background-color: ${Color.BACKGROUND_HOVER_OR_FOCUS};
+    switch (variant) {
+      case ButtonVariant.FILLED: {
+        return ({
+          border: "none",
+          "background-color": ButtonColors[color].COLOR,
+          color: "white"
+
+        })
+      }
+      case ButtonVariant.OUTLINED: {
+        return ({
+          "box-sizing": "border-box",
+          border: "1px solid",
+          color: ButtonColors[color].COLOR
+        })
+
+      }
+      default:
+    }
+  }}
+  &:hover, &:focus {
+    ${({color, variant}) => {
+      switch (variant) {
+        case ButtonVariant.FILLED: {
+          return ({
+            "background-color": ButtonColors[color][ButtonDetailColorTypes.HOVER_OR_FOCUS],
+          })
+        }
+        case ButtonVariant.OUTLINED: {
+          return ({
+            color: ButtonColors[color][ButtonDetailColorTypes.HOVER_OR_FOCUS]
+          })
+
+        }
+        default:
+      }
+    }}
+
   }
 
   &:active {
-    background-color: ${Color.BACKGROUND_ACTIVE};
+    ${({color, variant}) => {
+
+      switch (variant) {
+        case ButtonVariant.FILLED: {
+          return ({
+            "background-color": ButtonColors[color][ButtonDetailColorTypes.ACTIVE],
+          })
+        }
+
+        case ButtonVariant.OUTLINED: {
+          return ({
+            color: ButtonColors[color][ButtonDetailColorTypes.ACTIVE]
+          })
+
+        }
+        default:
+      }
+    }}
+
   }
 
   &:disabled {
-    background-color: ${Color.BACKGROUND_DISABLED};
     cursor: ${CursorVariants.NOT_ALLOWED};
+    ${({color, variant}) => {
+
+      switch (variant) {
+        case ButtonVariant.FILLED: {
+          return ({
+            "background-color": ButtonColors[color].DISABLED,
+
+          })
+        }
+        case ButtonVariant.OUTLINED: {
+          return ({
+            color: ButtonColors[color].DISABLED
+          })
+        }
+        default:
+      }
+    }}
   }
+
+
 `;
+
